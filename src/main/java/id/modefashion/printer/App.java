e668db2926614b9d1995088aa89077e002aaa8c9
daffa09 <daffa.fathan9@gmail.com>
2024-04-05 13:53:09 +0700

    Refactor printer initialization and add printer selection functionality

--- src/main/java/id/modefashion/printer/App.java
+++ src/main/java/id/modefashion/printer/App.java
@@ -5,6 +5,8 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.anastaciocintra.output.PrinterOutputStream;
+
 /**
  * MPOS Java Printer
  * 
@@ -12,16 +14,29 @@ import org.slf4j.LoggerFactory;
  */
 public class App {
   private static final Logger logger = LoggerFactory.getLogger(App.class);
-  public static void main(String[] args) {
 
+  public static void main(String[] args) {
     try {
-      logger.info("Initialize application....");
       PropertiesConfiguration config = new PropertiesConfiguration("printer.properties");
+      logger.info("Initialize application....");
+      logger.info("Printer use : " + config.getString("printer.name"));
+
+      if (config.getString("printer.name").isEmpty()) {
+        logger.info("Usage: java -jar xyz.jar (\"printer name\")");
+        logger.info("Printer list to use:");
+        String[] printServicesNames = PrinterOutputStream.getListPrintServicesNames();
+        for (String printServiceName : printServicesNames) {
+          logger.info(printServiceName);
+        }
+
+        System.exit(0);
+      }
+
       PrintServer server = new PrintServer(config);
       server.start();
       logger.info("Websocket server is runnig on port {}", config.getInt("printer.port"));
     } catch (ConfigurationException e) {
       e.printStackTrace();
-    } 
+    }
   }
 }
